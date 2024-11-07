dad3fb722e0ebc47aba753c3e9327b080369a34d
Paulo César Souza Pereira <drlund@bb.com.br>
2024-11-07 11:50:05 -0300

    Incluindo TypeORM para persistência de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
-import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ProdutoModule } from './produto/produto.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { MysqlConfigService } from './config/mysql.config.service';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: MysqlConfigService,
+      inject: [MysqlConfigService],
+    }),
+  ],
 })
 export class AppModule {}
